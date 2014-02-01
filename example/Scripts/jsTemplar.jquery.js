dcba01f2733f32d64a54d64719925d4b1ae38ebf
code-for-coffee <jtraverjr@gmail.com>
2014-02-01 15:30:12 -0600

    initial commit
    first commit

---
+++ example/Scripts/jsTemplar.jquery.js
@@ -0,0 +1,7 @@
+// jsTemplar.jquery.js
+// v0.1 - github.com/code-for-coffee
+// ------------------------------------------------------------
+function clearAndLoad(source) {
+    $("#content_body").empty();
+    $("#content_body").load(source);
+}
